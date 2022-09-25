7d5f58249e3446bb2021c751a13d383cdbe64a68
WCARobotics <55900422+WCARobotics@users.noreply.github.com>
2022-09-24 20:33:23 -0700

    Update teleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
@@ -7,9 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="Tele-Op")
 public class teleOp extends OpMode {
-    public static void main(String[] args) {
-        
-    }
+    
     public DcMotor leftFrontDrive, rightFrontDrive, leftBackDrive, rightBackDrive, armMotor1, armMotor2, carouselSpinner;
     public double speed, armspeed;
     public CRServo claw1, claw2;
