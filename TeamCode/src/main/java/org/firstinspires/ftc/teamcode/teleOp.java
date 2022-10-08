3eba823eee81530ea4c5cca03b471f9a124cf645
WCARobotics <55900422+WCARobotics@users.noreply.github.com>
2022-10-08 15:54:09 -0700

    new movment system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
@@ -1,52 +1,80 @@
 package org.firstinspires.ftc.teamcode;
 
+
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="Tele-Op")
 public class teleOp extends OpMode {
     
-    public DcMotor leftFrontDrive, rightFrontDrive, leftBackDrive, rightBackDrive;
-    public double speed;
-    //expansion hub 1 = arm1, port 2 = arm2
-    //claw 1 (right) = 5, claw 2  (left) = 1;
+    public DcMotor frontLeftWheel, frontRightWheel, backLeftWheel, backRightWheel;
+
     public void init() {
         //initializes the variables
-        leftFrontDrive = hardwareMap.get(DcMotor.class, "front left");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "front right");
-        leftBackDrive = hardwareMap.get(DcMotor.class, "back left");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "back right");
-        speed = 0;
+        frontLeftWheel = hardwareMap.get(DcMotor.class, "front left");
+        frontRightWheel = hardwareMap.get(DcMotor.class, "front right");
+        backLeftWheel = hardwareMap.get(DcMotor.class, "back left");
+        backRightWheel = hardwareMap.get(DcMotor.class, "back right");
+
         //set direction for motors
-        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        //Old code: left is forward and right is backward
+        //New code: right is forward and left is backwards
+        frontLeftWheel.setDirection(DcMotor.Direction.FORWARD);
+        frontRightWheel.setDirection(DcMotor.Direction.REVERSE);
+        backLeftWheel.setDirection(DcMotor.Direction.FORWARD);
+        backRightWheel.setDirection(DcMotor.Direction.REVERSE);
     }
 
     public void loop() {
         //move
-       speed=gamepad1.right_stick_y;
-       move(-speed,speed);
-       if(gamepad1.right_stick_x != 0){
-           if (gamepad1.right_stick_x < 0) {
-               move(speed / 2, speed);
-           }else{
-               move(speed, speed/2);
-           }
-       }
-       
+        double x = gamepad1.left_stick_x;
+        double y = -gamepad1.left_stick_y;
+        double turn = 0.0;
+        double theta = Math.atan2(y, x);
+        double power = Math.hypot(x,y);
+        double sin = Math.sin(theta - Math.PI/4);
+        double cos = Math.cos(theta - Math.PI/4);
+        double max = Math.max(Math.abs(sin), Math.abs(cos));
+        double[] wheels = new double[4];
+        if(gamepad1.left_bumper){
+            turn = -0.5;
+        }else if(gamepad1.right_bumper){
+            turn = 0.5;
+        }
 
+        for (int i = 0; i < wheels.length; i++) {
+            if(i%2 == 0){
+                wheels[i] = (power * (cos/max));
+                if(i == 0){
+                    wheels[i] -= turn;
+                }else{
+                    wheels[i] += turn;
+                }
+            }else{
+                wheels[i] = (power * (sin/max));
+                if(i == 3){
+                    wheels[i] -= turn;
+                }else{
+                    wheels[i] += turn;
+                }
+            }
+            if((power + Math.abs(turn))>1){
+                wheels[i] /= power+turn;
+            }
 
+        }
+        move(wheels);
 
     }
 
-    public void move(double powerLeft, double powerRight) {
-        leftFrontDrive.setPower(powerLeft);
-        leftBackDrive.setPower(powerLeft);
-        rightFrontDrive.setPower(powerRight);
-        rightBackDrive.setPower(powerRight);
+    public void move(double[] wheels) {
+        frontLeftWheel.setPower(wheels[1]);
+        backLeftWheel.setPower(wheels[2]);
+        frontRightWheel.setPower(wheels[0]);
+        backRightWheel.setPower(wheels[3]);
     }
 }
 
